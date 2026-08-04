993a008118892cbb3c0dee68f9b595ac9b57fb1b
Kai McCloud <aymeealvarezv@gmail.com>
2026-08-04 13:40:08 -0600

    feat: conditionally render AppHeader based on route path

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
 import AppHeader from './components/layout/AppHeader.vue'
+
+const route = useRoute()
+
+const showPublicHeader = computed(() => {
+  return !route.path.startsWith('/admin')
+})
 </script>
 
 <template>
   <div class="min-h-screen overflow-x-hidden">
-    <AppHeader />
+    <AppHeader v-if="showPublicHeader" />
 
     <RouterView />
   </div>
